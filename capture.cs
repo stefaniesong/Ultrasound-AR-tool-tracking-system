67baa318b09e6ce1a432cd9d7c1d76a87e0303bc
Stefanie <46021595+stefaniesong@users.noreply.github.com>
2022-05-13 21:38:34 -0400

    Add files via upload

--- capture.cs
+++ capture.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using System.IO;
-// Save the image the camera captured.
 
+// Save the image the camera captured.
 public class capture : MonoBehaviour
 {
+    public int resWidth = 1920;
+    public int resHeight = 1080;
     public static string path = "/Users/songguanyu/Desktop";
-    public GameObject M1;
+    public GameObject C1;
     public int num = 0;
 
     // Take a "screenshot" of a camera's Render Texture.
@@ -31,28 +33,35 @@ public class capture : MonoBehaviour
 
     void captureSlice(int n)
     {
-        Camera Cam = M1.GetComponent<Camera>();
+        Camera Cam = C1.GetComponent<Camera>();
 
-        RenderTexture RT = RenderTexture.active;
-        RenderTexture.active = Cam.targetTexture;
-
+        RenderTexture rendertex = new RenderTexture(resWidth, resHeight, 24);
+        Cam.targetTexture = rendertex;
         Cam.Render();
 
-        Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
-        Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        //RenderTexture RT = RenderTexture.active;
+        RenderTexture.active = Cam.targetTexture;
+
+        
+        //print(Cam.targetTexture);
+        // Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+        Texture2D Image = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+        // Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        Image.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
         Image.Apply();
-        RenderTexture.active = RT;
+        Cam.targetTexture = null;
+        RenderTexture.active = null;
 
         var Bytes = Image.EncodeToPNG();
-        File.WriteAllBytes(path + "/test_" + n + ".png", Bytes);
+        File.WriteAllBytes(path + "/test_"+ System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")+ ".png", Bytes);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Debug.Log("Save the image." + num);
             num++;
+            Debug.Log("Save the image." + num);
             captureSlice(num);
         }
     }
